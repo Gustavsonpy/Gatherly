07697bb98eece79b1030791c5b9304d2433c45ad
Gustavsonpy <gupaluski@gmail.com>
2026-08-08 15:12:20 -0300

    wip: config database

---
+++ project/API/Models/Models.cs
@@ -0,0 +1,22 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace API.Models
+{
+    public class User
+    {
+        public Guid Id { get; set; } = Guid.NewGuid();
+        public string Name { get; set; }
+        public string Username { get; set; }
+        public DateTime Birthday { get; set; }
+        public string HashPassword { get; set; }
+        public string Description { get; set; }
+        public string City { get; set; }
+        public string UrlPhoto { get; set; }
+        public bool IsSuspended { get; set; }
+        public bool  IsAdmin { get; set; }
+        public DateTime RegisterDate { get; set; }
+    }
+}
